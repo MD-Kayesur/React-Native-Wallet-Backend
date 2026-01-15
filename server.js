7f677b75d7f404cd2403aa6948bd1c0b15ec1a6e
MD-Kayesur <rmdkayesur@gmail.com>
2026-01-15 11:38:26 +0600

    done add rander api in rander enverronment and done deply

--- server.js
+++ server.js
@@ -5,9 +5,10 @@ import { neon } from "@neondatabase/serverless";
 import ratelimiter from "./src/middleware/rateLimiter.js";
 import transectionRoutes from "./src/routes/transectionRoutes.js";
 import { initDB } from "./src/config/db.js";
+import job from "./src/config/cron.js"; 
+dotenv.config();
 
- dotenv.config();
-
+if(process.env.NODE_ENV !== 'production')job.start();
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(ratelimiter);
@@ -17,7 +18,9 @@ app.use(express.json());
 const sql = neon(process.env.DATABASE_URL);
 // Initialize DB
 
-
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ message: "Server is running" , status:"ok"});
+});
 // Health check
 // app.get("/", (req, res) => {
 //   res.send("🚀 Server is running");
